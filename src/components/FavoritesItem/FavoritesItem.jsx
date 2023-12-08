72ab80af93d439a2f1c411235a059053eb6d1ce3
Hannah Bjorklund <bjorklundhannah@gmail.com>
2023-12-08 11:29:23 -0600

    Added some more styling for the search and favorites pages

--- src/components/FavoritesItem/FavoritesItem.jsx
+++ src/components/FavoritesItem/FavoritesItem.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function FavoritesItem({favorite}){
     console.log(favorite);
     return (
-        <span key={favorite.id}>
+        <div className='flexItem'>
             <img src={`${favorite.url}`} width={100} height={100}/>
-        </span>
+        </div>
     )
 }
